7ed47229064c4ffa86f67357e7f835d276d30620
Andrew Locke <alocke12992@gmail.com>
2018-03-08 10:49:21 -0700

    fin todoList

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Todo from './Todo'; 
 
 const TodoList = ({ todos, updateTodo, deleteTodo }) => (
-  <div>
-    I am da list bruh
+  <div className="row">
+    { todos.map( todo => 
+        <Todo 
+          key={todo.id}
+          {...todo}
+          updateTodo={updateTodo}
+          deleteTodo={deleteTodo}
+        /> 
+      )
+    }
+
   </div>
 )
 

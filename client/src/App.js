de352fe16c7d60917aa5e987481a88f6572f35c0
Andrew Locke <alocke12992@gmail.com>
2018-03-08 10:15:03 -0700

    frame app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList'
 
 class App extends Component {
+  state = { todos: [] }
+
+  componentDidMount () {
+    //TODO 
+  }
+
+  addItem = (name) => {
+    //TODO 
+  }
+
+
+  updateTodo = (id) => {
+    //TODO 
+  }
+
+
+  deleteTodo = (id) => {
+    //TODO 
+  }
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      
     );
   }
 }
